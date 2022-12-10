f14b68b9d0ea4277a4c33655c184e73eb4cc3d48
BatoolBtoush <batoolbtoush98@gmail.com>
2022-12-10 17:59:49 +0200

    new addition

--- favsongsapp/urls.py
+++ favsongsapp/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
-from .views import HomeFavView, AboutFavView, FavListView, FavDetailView, OpinionDetailView
+from .views import (
+    HomeFavView,
+    AboutFavView,
+    FavListView,
+    FavDetailView,
+    OpinionDetailView,
+    FavCreateView,
+    FavUpdateView,
+    FavDeleteView,
+)
 
 
 urlpatterns = [
-    path('fav-home', HomeFavView.as_view(), name='fav_home'),
-    path('fav-about', AboutFavView.as_view(), name='fav_about'),
-    path('fav-list', FavListView.as_view(), name='fav_list'),
-    path('fav-detail/<int:pk>', FavDetailView.as_view(), name='fav_detail'),
-    path('opinion-detail/<int:pk>', OpinionDetailView.as_view(), name='opinion_detail'),
-
-    
-
-
-
+    path("fav-home", HomeFavView.as_view(), name="fav_home"),
+    path("fav-about", AboutFavView.as_view(), name="fav_about"),
+    path("fav-list", FavListView.as_view(), name="fav_list"),
+    path("fav-detail/<int:pk>", FavDetailView.as_view(), name="fav_detail"),
+    path("opinion-detail/<int:pk>", OpinionDetailView.as_view(), name="opinion_detail"),
+    # path("fav-create/", FavCreateView.as_view(), name="fav_create"),
+    # path("<int:pk>/fav-update/", FavUpdateView.as_view(), name="fav_update"),
+    # path("<int:pk>/fav-delete/", FavDeleteView.as_view(), name="fav_delete"),
 ]
--- favsongsapp/views.py
+++ favsongsapp/views.py
@@ -1,24 +1,51 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic import (
+    TemplateView,
+    DetailView,
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Fav, Opinion
+
 # Create your views here.
 
+
 class HomeFavView(TemplateView):
-    template_name = '_base.html'
+    template_name = "_base.html"
+
 
 class AboutFavView(TemplateView):
-    template_name = 'about.html'
+    template_name = "about.html"
 
 
 class FavListView(ListView):
-    template_name = 'favlist.html'
+    template_name = "favlist.html"
     model = Fav
-    context_object_name = 'fav_list'
+    context_object_name = "fav_list"
+
 
 class FavDetailView(DetailView):
-    template_name = 'favdetail.html'
+    template_name = "favdetail.html"
     model = Fav
 
+
 class OpinionDetailView(DetailView):
-    template_name = 'opiniondetail.html'
+    template_name = "opiniondetail.html"
     model = Opinion
+
+
+class FavCreateView(CreateView):
+    template_name = "favcreate.html"
+    model = Fav
+
+
+class FavUpdateView(UpdateView):
+    template_name = "favupdate.html"
+    model = Fav
+
+
+class FavDeleteView(DeleteView):
+    template_name = "favdelete.html"
+    model = Fav
